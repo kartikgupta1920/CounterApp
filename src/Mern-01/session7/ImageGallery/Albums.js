0bb1c30d2fe279bca80bdd184795bdea3bae6577
Kartik <19kartikgupta@gmail.com>
2024-05-26 23:58:34 +0530

    card

--- src/Mern-01/session7/ImageGallery/Albums.js
+++ src/Mern-01/session7/ImageGallery/Albums.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {AppBar, Toolbar, Typography} from '@mui/material' ;
+import {AppBar, Box, Card, Toolbar, Typography} from '@mui/material' ;
 
 const Albums = () => {
     const [albums, setAlbums] = useState([]);
@@ -18,21 +18,26 @@ const Albums = () => {
     }, []);
     return(
          <div>
-            <AppBar position="static">
+            <AppBar position="static" sx={{backgroundColor: '#2b2b2b'}}>
                 <Toolbar variant="dense">
                     <Typography variant="h6" color="inherit" component="div">
                         Image Gallery
                     </Typography>
                 </Toolbar>
             </AppBar>
+            <Box sx={{display: 'flex', alignItems: 'center', flexWrap: 'wrap'}}>
             {albums.map((album) => {
                 return(
-                    <Link to={`photos/${album.id}`} key={album.id}>
-                        <p>Album Id: {album.id}</p>
-                        <p>Album Title: {album.title}</p>
-                    </Link>
+                        <Card variant="outlined" sx={{ padding: '10px', width: '100px' }}>
+                            <Link to={`photos/${album.id}`} key={album.id}>
+                                <p>Album Id: {album.id}</p>
+                                <p>Album Title: {album.title}</p>
+                            </Link>
+                        </Card>
+                    
                 );
             })}
+            </Box>
             </div>
     );
 };
